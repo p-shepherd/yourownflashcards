a222d39d9b357a79deceae2e2c076e29ac8bf8d6
Paul Shepherd <paul.shepherd.programming@gmail.com>
2024-01-05 22:09:21 +0100

    Updated files for new repository

--- firebase.js
+++ firebase.js
@@ -4,15 +4,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAR039iEeX1v34XBptfqz_5sAkhuHn0oDI",
-  authDomain: "flashcards-app-7a15d.firebaseapp.com",
-  projectId: "flashcards-app-7a15d",
-  storageBucket: "flashcards-app-7a15d.appspot.com",
-  messagingSenderId: "579118308229",
-  appId: "1:579118308229:web:0140309fdada2d973202ec",
-  measurementId: "G-5DHEYZT61X",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
